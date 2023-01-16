8bf415bf557f728d46908ade574552e70f38ca57
Javier Flores <jffc.dev@gmail.com>
2023-01-16 00:06:10 -0500

    feat(App): Main root endpoint added

--- api/index.js
+++ api/index.js
@@ -3,6 +3,15 @@ import leaderboard from '../db/leaderboard.json'
 
 const app = new Hono()
 
+app.get('/', (ctx) => {
+  return ctx.json([
+    {
+      endpoint: '/leaderboard',
+      description: 'Returns the leaderboard'
+    }
+  ])
+})
+
 app.get('/leaderboard', (ctx) => {
   return ctx.json(leaderboard)
 })

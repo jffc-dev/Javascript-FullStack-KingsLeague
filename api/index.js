56a93b5cb5b6b9ea7fa0304858dd709ee463dbad
Javier Flores <jffc.dev@gmail.com>
2023-01-19 03:54:28 -0500

    feat(App): Team and presidents endpoints added

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import { Hono } from 'hono'
 import { serveStatic } from 'hono/serve-static.module'
 import leaderboard from '../db/leaderboard.json'
+import presidents from '../db/presidents.json'
+import teams from '../db/teams.json'
 
 const app = new Hono()
 
@@ -8,13 +10,45 @@ app.get('/', (ctx) => {
   return ctx.json([
     {
       endpoint: '/leaderboard',
-      description: 'Returns the leaderboard'
+      description: 'Returns the Kings League leaderboard'
+    },
+    {
+      endpoint: '/teams',
+      description: 'Returns the Kings League teams'
+    },
+    {
+      endpoint: '/presidents',
+      description: 'Returns the Kings League presidents'
     }
   ])
 })
 
 app.get('/leaderboard', (ctx) => {
   return ctx.json(leaderboard)
+})
+
+app.get('/teams', (ctx) => {
+  return ctx.json(teams)
+})
+
+app.get('/teams/:id', (ctx) => {
+  const id = ctx.req.param('id')
+  const foundTeam = teams.find(team => team.id === id)
+  return foundTeam
+    ? ctx.json(foundTeam)
+    : ctx.json({ message: 'Team not found' }, 404)
+})
+
+app.get('/presidents', (ctx) => {
+  return ctx.json(presidents)
+})
+
+app.get('/presidents/:id', (ctx) => {
+  const id = ctx.req.param('id')
+  const foundPresident = presidents.find(president => president.id === id)
+  return foundPresident
+    ? ctx.json(foundPresident)
+    : ctx.json({ message: 'President not found' }, 404)
 })
 
 app.get('/static/*', serveStatic({ root: './' }))

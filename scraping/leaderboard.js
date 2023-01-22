c4048525af08347aec2f1a29bd6fe5f11b0d6fea
Javier Flores <jffc.dev@gmail.com>
2023-01-22 17:36:00 -0500

    feat(App): Utils functions used

--- scraping/leaderboard.js
+++ scraping/leaderboard.js
@@ -1,10 +1,5 @@
 import * as cheerio from 'cheerio'
-import { writeFile, readFile } from 'node:fs/promises'
-import path from 'node:path'
-
-const DB_PATH = path.join(process.cwd(), 'db')
-const TEAMS = await readFile(`${DB_PATH}/teams.json`, 'utf-8').then(JSON.parse)
-// import TEAMS from '../db/teams.json' assert {type: 'json'} Not supported by linter
+import { writeDBFile, TEAMS, PRESIDENTS } from './utils.js'
 
 const URLS = {
   leaderboard: 'https://kingsleague.pro/estadisticas/clasificacion/'
@@ -30,12 +25,22 @@ const getLeaderBoard = async (url) => {
   const $ = await scrape(url)
   const $rows = $('table tbody tr')
 
-  const cleanText = text => text
-    .replace(/\t|\n|\s:/g, '')
-    .replace(/.*:/g, ' ')
-    .trim()
+  const cleanText = (text) =>
+    text
+      .replace(/\t|\n|\s:/g, '')
+      .replace(/.*:/g, ' ')
+      .trim()
 
-  const getTeamFromName = ({ name }) => TEAMS.find(team => team.name === name)
+  const getTeamFromName = ({ name }) => {
+    const { presidentId, ...restOfTeamInfo } = TEAMS.find(
+      (team) => team.name === name
+    )
+    const president = PRESIDENTS.find(
+      (president) => president.id === presidentId
+    )
+
+    return { ...restOfTeamInfo, president }
+  }
 
   const leaderBoardSelectorEntries = Object.entries(LEADERBOARD_SELECTORS)
   const leaderBoard = []
@@ -43,18 +48,19 @@ const getLeaderBoard = async (url) => {
   $rows.each((_, el) => {
     const $el = $(el)
 
-    const leaderBoardEntries = leaderBoardSelectorEntries.map(([key, { selector, typeOf }]) => {
-      const rawValue = $el.find(selector).text()
-      const cleanedValue = cleanText(rawValue)
+    const leaderBoardEntries = leaderBoardSelectorEntries.map(
+      ([key, { selector, typeOf }]) => {
+        const rawValue = $el.find(selector).text()
+        const cleanedValue = cleanText(rawValue)
 
-      const value = typeOf === 'number'
-        ? Number(cleanedValue)
-        : cleanedValue
+        const value = typeOf === 'number' ? Number(cleanedValue) : cleanedValue
 
-      return [key, value]
-    })
+        return [key, value]
+      }
+    )
 
-    const { team: teamName, ...leaderBoardForTeam } = Object.fromEntries(leaderBoardEntries)
+    const { team: teamName, ...leaderBoardForTeam } =
+      Object.fromEntries(leaderBoardEntries)
 
     const team = getTeamFromName({ name: teamName })
 
@@ -67,7 +73,7 @@ const getLeaderBoard = async (url) => {
   return leaderBoard
 }
 
-const leaderBoard = await getLeaderBoard(URLS.leaderboard)
+const leaderdoard = await getLeaderBoard(URLS.leaderboard)
 // console.log(import.meta.url)
 // console.log(path.join(process.cwd()))
-await writeFile(`${DB_PATH}/leaderboard.json`, JSON.stringify(leaderBoard, null, 2), 'utf-8')
+await writeDBFile('leaderboard', leaderdoard)

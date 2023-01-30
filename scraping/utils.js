9014196fc1b235fafd389e52971500abde7936be
Javier Flores <jffc.dev@gmail.com>
2023-01-30 11:59:54 -0500

    feat(App): Log file added

--- scraping/utils.js
+++ scraping/utils.js
@@ -1,16 +1,35 @@
-import { writeFile, readFile } from 'node:fs/promises'
+import { writeFile, readFile, appendFile } from 'node:fs/promises'
 import path from 'node:path'
 
 const DB_PATH = path.join(process.cwd(), 'db')
+const DB_LOGS = path.join(process.cwd(), 'scraping/logs')
 
-function readDBFile(fileName) {
+const readDBFile = (fileName) => {
   return readFile(`${DB_PATH}/${fileName}.json`, 'utf-8').then(JSON.parse)
 }
 
 export const TEAMS = await readDBFile('teams')
 export const PRESIDENTS = await readDBFile('presidents')
 
-export function writeDBFile(dbName, data) {
+const dateFormat = (date) => {
+  return ('00' + date.getDate()).slice(-2) + '/' +
+  ('00' + (date.getMonth() + 1)).slice(-2) + '/' +
+  date.getFullYear() + ' ' +
+  ('00' + date.getHours()).slice(-2) + ':' +
+  ('00' + date.getMinutes()).slice(-2) + ':' +
+  ('00' + date.getSeconds()).slice(-2)
+}
+
+const writeLog = async(dbName) => {
+  const msg = `Leaderboard data was successfully scrapped ${dateFormat(new Date())}.\n`
+  return appendFile(`${DB_LOGS}/${dbName}.txt`, msg, (err) => {
+    if (err) throw err
+    console.log('Saved!')
+  })
+}
+
+export const writeDBFile = async(dbName, data) => {
+  await writeLog(dbName)
   return writeFile(
     `${DB_PATH}/${dbName}.json`,
     JSON.stringify(data, null, 2),
